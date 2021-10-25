a5e9a0002e8a70779c5ac417a80457c0c9c46708
DecentWaterBottle <jack.carter@mycit.ie>
2021-10-25 14:03:05 +0100

    Added start of transfer function. Added test code

--- contracts/MediCoin.sol
+++ contracts/MediCoin.sol
@@ -6,6 +6,9 @@ contract MediCoin{
     string public name = "MediCoin";
     // Symbol
     string public symbol = "MEDI";
+    // Standard
+    string public standard = "Medi Coin v1.0";
+
 
     uint256 public totalSupply;
 
@@ -15,5 +18,13 @@ contract MediCoin{
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
         
+    }
+
+    //Transfer
+    function transfer(address _to, uint256 _value) public returns(bool success){
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
     }
 }
--- test/MediCoin.js
+++ test/MediCoin.js
@@ -9,8 +9,12 @@ contract("MediCoin", function(accounts){
             return tokenInstance.name();
         }).then(function(name){
             assert.equal(name, 'MediCoin', 'has the correct name');
+            return tokenInstance.symbol();
         }).then(function(symbol){
             assert.equal(symbol,"MEDI", "has the correct symbol");
+            return tokenInstance.standard();
+        }).then(function(standard){
+            assert.equal(standard, "Medi Coin v1.0", "has the correct standard");
         });
     })
 
@@ -22,7 +26,27 @@ contract("MediCoin", function(accounts){
             assert.equal(totalSupply.toNumber(), 1000000000, "sets the total supply to 1,000,000,000");
             return tokenInstance.balanceOf(accounts[0]);
         }).then(function(adminBalance){
-            assert.equal(adminBalance.toNumber(), 1000000000, "it allocates the initial supply to the admin account")
+            assert.equal(adminBalance.toNumber(), 1000000000, "it allocates the initial supply to the admin account");
+        });
+    });
+
+    it("transfers token ownership", function(){
+        return MediCoin.deployed().then(function(instance){
+            tokenInstance = instance;
+            // Test by sending a larger amount than the sender's balance
+            return tokenInstance.transfer.call(accounts[1], 9999999999999);
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf("revert") >= 0, "error message must contain revert");
+            return tokenInstance.transfer(accounts[1], 500000000, { from: accounts[0]});
+        }).then (function(receipt){
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 500000000, "adds the amount to the receiving account");
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 500000000, "deducts the amount from the sending account");
         });
     });
 });
+
+//134:14
