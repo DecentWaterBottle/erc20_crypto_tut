7154bc03ed0e1fd306634e26bdfc268c41ada502
DecentWaterBottle <jack.carter@mycit.ie>
2021-10-28 13:34:41 +0100

    Added files for token sale.

---
+++ contracts/MediCoinSale.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.9;
+
+
+contract MediCoinSale {
+    address admin;
+    constructor() {
+        admin = msg.sender;
+        // Token Contract
+        // Token Price
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const MediCoin = artifacts.require("./MediCoin.sol");
+var MediCoinSale = artifacts.require("./MediCoinSale.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(MediCoin, 1000000000);
+  deployer.deploy(MediCoinSale);
 };
