64440ba13292ac7f9d2663fe5eb9889fcc53b331
DecentWaterBottle <jack.carter@mycit.ie>
2021-10-29 01:29:48 +0100

    Completed buyTokens and endSale functions

--- contracts/MediCoinSale.sol
+++ contracts/MediCoinSale.sol
@@ -1,11 +1,37 @@
 pragma solidity ^0.8.9;
-
+import "./MediCoin.sol";
 
 contract MediCoinSale {
     address admin;
-    constructor() {
+    uint256 public coinPrice;
+    MediCoin public tokenContract;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
+
+    constructor(MediCoin _tokenContract, uint256 _coinPrice) {
         admin = msg.sender;
-        // Token Contract
-        // Token Price
+        coinPrice = _coinPrice;
+        tokenContract = _tokenContract;
+    }
+
+    // Safe Multiplication from DS-Math
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+    // Token Buying
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, coinPrice));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+        tokensSold += _numberOfTokens;
+        emit Sell(msg.sender, _numberOfTokens);
+    }
+
+    function endSale() public {
+        require(msg.sender == admin);
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
+        // address payable admin_addr = payable(address(admin));
+        selfdestruct(payable(admin));
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@ const MediCoin = artifacts.require("./MediCoin.sol");
 var MediCoinSale = artifacts.require("./MediCoinSale.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(MediCoin, 1000000000);
-  deployer.deploy(MediCoinSale);
+  deployer.deploy(MediCoin, 1000000000).then(function(){
+    return deployer.deploy(MediCoinSale, MediCoin.address, 1000000000000000);
+  });
 };
